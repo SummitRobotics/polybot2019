836f0a32d61026f9a70cefcbc563b77a4f2e0849
Unknown <aidanb1049@gmail.com>
2019-01-14 22:09:15 -0800

    Hotfixes again

--- src/main/java/frc/robot/commands/TestMast.java
+++ src/main/java/frc/robot/commands/TestMast.java
@@ -1,9 +1,12 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class TestMast extends Command implements CommandInterface{
-    private double power, time, timestamp;
+    private double power, time;
+    private double currentTime, timeError;
 
     public TestMast(double power, double time){
         requires(subsystems.revController);
@@ -14,19 +17,21 @@ public class TestMast extends Command implements CommandInterface{
 
     @Override
     protected void initialize() {
-        
+        currentTime = Timer.getFPGATimestamp();
     }
 
     @Override
     protected void execute() {
-        while(!isFinished()){
+        timeError = Timer.getFPGATimestamp() - currentTime;
+        while(true){
             subsystems.revController.sparkMaxTest.set(power);
+            SmartDashboard.putNumber("Power", power);
         }
     }
 
     @Override
     protected boolean isFinished() {
-        return timeSinceInitialized() < time;
+        return timeError == time; 
     }
 
     @Override
